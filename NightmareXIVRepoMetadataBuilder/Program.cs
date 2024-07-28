99761143e751f61175b44272c06590e3ba792fb5
Limiana <5073202+Limiana@users.noreply.github.com>
2024-07-28 05:49:03 +0300

    Update

--- NightmareXIVRepoMetadataBuilder/Program.cs
+++ NightmareXIVRepoMetadataBuilder/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace NightmareXIVRepoMetadataBuilder;
+﻿using Octokit.Internal;
+using Octokit;
+using System.Text;
+
+namespace NightmareXIVRepoMetadataBuilder;
+
+#nullable disable
 
 internal class Program
 {
@@ -8,10 +14,41 @@ internal class Program
         var envvars = (object[])[.. Environment.GetEnvironmentVariables().Keys];
         Console.WriteLine($"Existing envvars: {string.Join(", ", envvars.Select(x => x.ToString()))}");
         Key = Environment.GetEnvironmentVariable("GH_TOKEN") ?? throw new NullReferenceException("Token is null");
-        foreach(var param in ParseParams())
+        var pars = ParseParams();
+        foreach(var param in pars)
         {
             Console.WriteLine($"{param.Key}={param.Value}");
         }
+
+        var currentRepo = Environment.GetEnvironmentVariable("GITHUB_REPOSITORY").Split("/");
+
+        var roCredentials = new InMemoryCredentialStore(new(Key, AuthenticationType.Bearer));
+        var github = new GitHubClient(new ProductHeaderValue("NightmareXIVMetadataUpdater"), roCredentials);
+        var repo = github.Repository.Get(currentRepo[0], currentRepo[1]).Result;
+
+        StringBuilder builder = new();
+        builder.Append($"# {repo.Name}\n");
+
+        if(pars.TryGetValue("description", out var v) && v == "false")
+        {
+            //
+        }
+        else
+        {
+            builder.Append($"{repo.Description}\n");
+        }
+
+        foreach(var param in pars)
+        {
+            var text = github.Connection.GetHtml(new(GetURL(param.Key, param.Value)), new Dictionary<string, string>()).Result.Body;
+            builder.Append(text);
+            if(!text.EndsWith("\n"))
+            {
+                builder.Append("\n");
+            }
+        }
+
+        File.WriteAllText("README.md", builder.ToString(), Encoding.UTF8);
     }
 
     static string GetURL(string param, string type)
