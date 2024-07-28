5e851df8a0c3d51e56f303a545adb9f1086ad977
Limiana <5073202+Limiana@users.noreply.github.com>
2024-07-28 05:19:00 +0300

    Add project files.

---
+++ NightmareXIVRepoMetadataBuilder/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace NightmareXIVRepoMetadataBuilder;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        foreach(var param in ParseParams())
+        {
+            Console.WriteLine($"{param.Key}={param.Value}");
+        }
+    }
+
+    static string GetURL(string param, string type)
+    {
+        return $"https://github.com/NightmareXIV/MyDalamudPlugins/raw/main/meta/{param}/{type}.md";
+    }
+
+    static Dictionary<string, string> ParseParams()
+    {
+        var file = File.ReadAllText("meta/config.ini").Split("\n");
+        var ret = new Dictionary<string, string>();
+        foreach(var line in file)
+        {
+            var s = line.Trim().Split("=");
+            if(s.Length == 1)
+            {
+                ret.Add(s[0], s[1]);
+            }
+        }
+        return ret;
+    }
+}
